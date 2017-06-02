20b1e7d03f7a80d662eb15af5e342f21e97a2b08
bamford <stephen.bamford@res-ltd.com>
2017-06-02 13:52:24 +0100

    Add missing unit tests.

---
+++ UnorderedListStartLineParserElementTest.cs
@@ -0,0 +1,35 @@
+﻿using Xunit;
+
+namespace Markdown
+{
+    public class UnorderedListStartLineParserElementTest
+    {
+        private const string LIST_START_TEXT = "<ul>";
+        private UnorderedListStartLineParserElement _sut;
+
+        public UnorderedListStartLineParserElementTest()
+        {
+            _sut = new UnorderedListStartLineParserElement();
+        }
+
+        [Fact]
+        public void Parse_not_in_list_adds_start_tag_before_text()
+        {
+            bool inListAfter;
+            bool inListBefore = false;
+            string lineText = "some line";
+            string expectedText = $"{LIST_START_TEXT}{lineText}";
+            Assert.Equal(expectedText, _sut.ParseElement(lineText, inListBefore, out inListAfter));
+        }
+
+        [Fact]
+        public void Parse_in_list_does_not_add_start_tag_before_text()
+        {
+            bool inListAfter;
+            bool inListBefore = true;
+            string lineText = "some line";
+            string expectedText = lineText;
+            Assert.Equal(expectedText, _sut.ParseElement(lineText, inListBefore, out inListAfter));
+        }
+    }
+}
